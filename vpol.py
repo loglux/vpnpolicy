11f1db67b95c6e613e20c5d04caabce18ec4fff4
loglux <call2vlad@outlook.com>
2020-12-19 23:07:08 +0000

    New version

--- vpol.py
+++ vpol.py
@@ -9,17 +9,13 @@ class VPN_Rules():
         self.pattern = """^(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)$"""
         self.static_lines = []
         self.vpn_list = []
-        #print("Hi")
 
     def domains(self):
-        #print(self.local_ip)
         lines = filter(None, open("domains.txt", "r").read().splitlines())
-        #vpn_list = []
         for d in lines:
             results = pydig.query(d, 'A')
             results = [item.replace('\r', '') for item in results]
             results = [x for x in results if re.match(self.pattern, x)]
-            #print(results)
             for r in results:
                 req = '<' + d[:10] + '>' + self.local_ip + '>' + r + '>VPN'
                 self.vpn_list.append(req)
@@ -27,14 +23,12 @@ class VPN_Rules():
 
     def static(self):
         try:
-    #        static_lines = []
             with open("static.csv", mode="r") as f:
                 csv_lines = csv.reader(f)
                 for line in csv_lines:
                     if len(line) == 4:
                         line = '<' + str(line[0]).strip()[:10] + '>' + str(line[1]).strip() + '>' + str(line[2]).strip() + '>' + str(line[3]).strip().upper()
                         self.static_lines.append(line)
-            #return self.static_lines
             print(self.static_lines)
         except FileNotFoundError:
             pass
@@ -42,10 +36,8 @@ class VPN_Rules():
     def all_rules(self, seq=1):
         if self.static_lines:
             self.vpn_list = self.vpn_list + self.static_lines
-        #print(self.vpn_list)
         self.vpn_list = ''.join(self.vpn_list)
         self.vpn_list = "nvram set vpn_client" + str(seq) + "_clientlist=" + '"' + self.vpn_list + '"'
-        #print(vpn_list)
 
     def unset_nvram(self, num=''):
         for n in ['', 1, 2, 3, 4, 5]:
@@ -69,8 +61,9 @@ class VPN_Rules():
         #os.system(service_restart)
 
 if __name__ == '__main__':
-    local = "0.0.0.0"
-    client = 2
+    local = "0.0.0.0" # your local subnet or network node
+    client = 2 # 1,2,3,4 or 5
+    #configs = '/opt/home/vpnpolicy'
     rules = VPN_Rules(local)
     rules.domains()
     rules.static()
