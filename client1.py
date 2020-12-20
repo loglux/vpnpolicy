51be8f0937f46a9e570e0f42874e8b87a81b1a46
loglux <call2vlad@outlook.com>
2020-12-20 10:48:46 +0000

    a new version

---
+++ client1.py
@@ -0,0 +1,17 @@
+from vpol import VPN_Rules
+
+if __name__ == '__main__':
+    local = "0.0.0.0" # your local subnet or network node
+    client = 1 # 1,2,3,4 or 5
+    conf_path = ""
+    d_conf = conf_path + "domains.txt"
+    s_conf = conf_path + "static.csv"
+    rules = VPN_Rules(local)
+    rules.domains(d_conf)
+    rules.static(s_conf)
+    rules.all_rules(client)
+    rules.unset_nvram()
+    rules.unset_nvram(client)
+    rules.set_nvram()
+    #rules.nvram_commit()
+    rules.client_restart(client)
